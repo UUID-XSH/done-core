5426c7c1e2dab2f7aebf0b3e07404316f8c5f558
aristark <aristark@126.com>
2016-11-28 14:46:30 +0800

    first commit

---
+++ src/main/java/com/done/Application.java
@@ -0,0 +1,14 @@
+package com.done;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xiaohuo on 16/11/28.
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
