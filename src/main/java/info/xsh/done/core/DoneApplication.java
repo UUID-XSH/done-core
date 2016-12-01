b8f67f18dc2ee553a4363e42d7c7193a47adac61
yangxueying <330110432@qq.com>
2016-12-01 13:40:56 +0800

    添加夏天的小工具~

--- src/main/java/info/xsh/done/core/DoneApplication.java
+++ src/main/java/info/xsh/done/core/DoneApplication.java
@@ -1,5 +1,6 @@
 package info.xsh.done.core;
 
+import info.yannxia.java.chameleon.SpringConvertFactoryImplLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,10 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /**
  * Created by xiaohuo on 16/11/28.
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = SpringConvertFactoryImplLoader.class)
 @EnableJpaRepositories
 public class DoneApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DoneApplication.class,args);
+        SpringApplication.run(DoneApplication.class, args);
     }
 }
