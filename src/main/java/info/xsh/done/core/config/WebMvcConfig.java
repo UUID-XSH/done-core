7f008cbfea0ce962ac6c072b5dcf051e7fba61a3
yann <me.yan.xia@qq.com>
2016-12-05 14:40:49 +0800

    增加跨域

---
+++ src/main/java/info/xsh/done/core/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package info.xsh.done.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by yann on 2016/12/5.
+ */
+@Configuration
+public class WebMvcConfig {
+
+    /**
+     * 增加跨域配置
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
+    }
+}
