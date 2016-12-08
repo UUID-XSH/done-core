ddf1d3e1e96e08a66201f96fb1390fa6206d261d
yangxueying <330110432@qq.com>
2016-12-08 18:01:28 +0800

    哈

--- src/main/java/info/xsh/done/core/controller/vo/UserVo.java
+++ src/main/java/info/xsh/done/core/controller/vo/UserVo.java
@@ -12,26 +12,26 @@ import java.time.LocalDateTime;
  * Created by yangxueying on 2016/12/1.
  */
 @Data
-@UserCreateFormAnnotation
+@UserCreateFormAnnotation(message = "UserVo校验错误")
 public class UserVo {
 
     private Long id; //用户id
 
-    @NotEmpty(message = "1111")
+    @NotEmpty(message = "用户名不能为空")
     private String name; //用户名
 
-    @NotEmpty(message = "1111")
+    @NotEmpty(message = "用户密码不能为空")
     private String password; //用户密码
 
-    @NotEmpty
+    @NotEmpty(message = "用户重复密码不能为空")
     private String passwordRepeated;
 
     private String nickName;
 
-    @NotEmpty
+    @NotEmpty(message = "email不能为空")
     private String email;
 
-    @NotNull
+    @NotNull(message = "用户角色不能为空")
     private Role role = Role.USER;
 
     private LocalDateTime registerDate; // 创建时间
